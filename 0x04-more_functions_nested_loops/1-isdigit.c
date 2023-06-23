acccfd8ccbfedf125f7edf847f53e1c576721a66
Thapelo01 <thapelomokwena02@gmail.com>
2023-06-23 03:51:06 -0700

    Nested

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isupper - Checks if a char is a digit
- *@x: The # to be checked
+ * _isdigit - Checks if a char is a digit
+ *@c: The # to be checked
  * Return: 1 for a char that will be a digit or 0 for anything
  */
 int _isdigit(int c)
