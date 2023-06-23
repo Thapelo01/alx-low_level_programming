8af865a3b990ed47a9647e245947103b31fe5a4b
Thapelo01 <thapelomokwena02@gmail.com>
2023-06-23 03:20:43 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,12 +22,12 @@ void print_remaining_days(int month, int day, int year);
 	{
 	if (month == 2 && day == 60)
 	{
-	printf("Day of the year: %d\n", month, day - 31, year);
+	printf("Invalid: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
 	printf("Day of the year: %d\n", day);
-	printf("Remaining year: %d\n", 366 - day);
+	printf("Remaining year: %d\n", 365 - day);
 	}
 	}
 }
