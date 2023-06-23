b224dfe0d794a3ad8809215d42e1e538e08b220f
Thapelo01 <thapelomokwena02@gmail.com>
2023-06-23 06:39:23 -0700

    Nested

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_diagona(0);
+	print_diagona(2);
+	print_diagona(10);
+	print_diagona(-4);
+	return (0);
+}
