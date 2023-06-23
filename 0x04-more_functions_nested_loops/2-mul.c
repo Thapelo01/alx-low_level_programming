82dcd4bdfe7055edd7d69003de36d3e42b6835d7
Thapelo01 <thapelomokwena02@gmail.com>
2023-06-23 03:58:16 -0700

    Nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiplies two #
+ *@x: The first # to be multiply
+ *@y: The first # to be multiply with first #
+ * Return: The result of multiplication between x param and y param
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
